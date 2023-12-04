1ca55e90c82fe2e37c5bd12fa353d5a48962e6ce
George Lazarov <122516587+Moramarth@users.noreply.github.com>
2023-12-04 06:31:03 +0200

    Basic logic handled
    cards are hidden on creation, guessed paris stay flipped

--- Demo - Memory game/src/App.vue
+++ Demo - Memory game/src/App.vue
@@ -1,13 +1,64 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Card from './components/Card.vue';
 
 export default {
   components: {
-    HelloWorld,
+    Card,
+  },
+  data() {
+    return {
+      cards: [
+        { idx: 1, type: 'Vue', img: 'https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg' },
+        { idx: 2, type: 'React', img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png' },
+        { idx: 3, type: 'Vue', img: 'https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg' },
+        { idx: 4, type: 'React', img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png' },
+      ],
+      selectedItem: [],
+      guessedItems: [],
+    };
+  },
+  watch: {
+    selectedItem: {
+      handler(newVal) {
+        if (newVal.length === 2 && (newVal[0].type === newVal[1].type))
+          this.guessedItems.push(newVal[0].type);
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    handleSelect(idx, type) {
+      if (this.selectedItem.length < 2)
+        this.selectedItem.push({ idx, type });
+
+      else if (this.selectedItem.length === 2)
+        this.selectedItem = [{ idx, type }];
+    },
   },
 };
 </script>
 
 <template>
-  <HelloWorld />
+  <div class="myGrid">
+    <Card
+      v-for="card in cards"
+      :key="card.idx"
+      :type="card.type"
+      :img="card.img"
+      :idx="card.idx"
+      :active-items="selectedItem"
+      :guessed-items="guessedItems"
+      @on-click="handleSelect"
+    />
+  </div>
 </template>
+
+<style scoped>
+.myGrid {
+  max-width: 400px;
+  margin: 2rem auto;
+  display: grid;
+  grid-template-columns: repeat(2, minmax(50px, 1fr));
+  gap: 1rem;
+}
+</style>
---
+++ Demo - Memory game/src/components/Card.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+  props: {
+    img: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    idx: {
+      type: Number,
+      required: true,
+    },
+    activeItems: {
+      type: Array,
+    },
+    guessedItems: {
+      type: Array,
+    },
+  },
+  emits: ['onClick'],
+  computed: {
+    isActive() {
+      const selected = this.activeItems.map(i => i.idx);
+      return this.guessedItems.includes(this.type) || selected.includes(this.idx);
+    },
+  },
+  methods: {
+    onSelect() {
+      if (!this.isActive)
+        this.$emit('onClick', this.idx, this.type);
+    },
+  },
+};
+</script>
+
+<template>
+  <article class="card" @click="onSelect">
+    <template v-if="isActive">
+      <img :src="img" alt="card-face">
+      <h2> {{ type }}</h2>
+    </template>
+    <img v-else src="https://opengameart.org/sites/default/files/card%20back%20black.png" alt="">
+  </article>
+</template>
+
+<style scoped>
+.card {
+    margin: 0;
+    text-align: center;
+}
+</style>
