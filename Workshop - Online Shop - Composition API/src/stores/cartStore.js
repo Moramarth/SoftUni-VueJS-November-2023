2bd4197de8f60d96ee9caa8b45b449fbaa6513e8
George Lazarov <122516587+Moramarth@users.noreply.github.com>
2024-01-22 22:31:29 +0200

    Update Stores to Composition APi

--- Workshop - Online Shop - Composition API/src/stores/cartStore.js
+++ Workshop - Online Shop - Composition API/src/stores/cartStore.js
@@ -1,36 +1,39 @@
 import { defineStore } from 'pinia';
+import { computed, ref } from 'vue';
 
-export const useCartStore = defineStore('cart', {
-  state: () => ({
-    products: [],
-  }),
-  getters: {
-    getProduct: (state) => {
-      return productId => state.products.find(prod => prod.id === productId);
-    },
-  },
-  actions: {
-    addToCart(selectedId) {
-      const productInCart = this.products.find(prod => prod.id === selectedId);
-      if (!productInCart) {
-        this.products.push({ id: selectedId, quantity: 1 });
-      }
-      else {
-        productInCart.quantity += 1;
-      }
-    },
-    changeQuantity(productId, event) {
-      const qty = Number(event.target.value) ?? 0;
-      const productInCart = this.products.find(prod => prod.id === productId);
-      if (!productInCart)
-        return;
+export const useCartStore = defineStore('cart', () => {
+  const products = ref([]);
 
-      if (qty > 0) {
-        productInCart.quantity = qty;
-      }
-      else {
-        this.products = this.products.filter(prod => prod.id !== productId);
-      }
-    },
-  },
+  const getProduct = computed(() => productId => products.value.find(prod => prod.id === productId));
+
+  function addToCart(selectedId) {
+    const productInCart = products.value.find(prod => prod.id === selectedId);
+    if (!productInCart) {
+      products.value.push({ id: selectedId, quantity: 1 });
+    }
+    else {
+      productInCart.quantity += 1;
+    }
+  };
+
+  function changeQuantity(productId, event) {
+    const qty = Number(event.target.value) ?? 0;
+    const productInCart = products.value.find(prod => prod.id === productId);
+    if (!productInCart)
+      return;
+
+    if (qty > 0) {
+      productInCart.quantity = qty;
+    }
+    else {
+      products.value = products.value.filter(prod => prod.id !== productId);
+    }
+  };
+
+  return {
+    products,
+    getProduct,
+    addToCart,
+    changeQuantity,
+  };
 });
--- Workshop - Online Shop - Composition API/src/stores/userStore.js
+++ Workshop - Online Shop - Composition API/src/stores/userStore.js
@@ -1,36 +1,47 @@
 import { defineStore } from 'pinia';
+import { ref } from 'vue';
 
-export const useUserStore = defineStore('user', {
-  state: () => {
-    return {
-      isAuthenticated: false,
-      profile: null,
-      favouritesIDs: [],
-    };
-  },
-  actions: {
-    setProfile(profileData) {
-      this.profile = profileData;
-      this.isAuthenticated = true;
-      sessionStorage.setItem('user-profile', JSON.stringify(profileData));
-    },
-    addFavouriteProduct(id) {
-      this.favouritesIDs.push(id);
-    },
-    removeFavouriteProduct(id) {
-      this.favouritesIDs = this.favouritesIDs.filter(favourite => favourite !== id);
-    },
-    getPersistedProfile() {
-      const persisted = sessionStorage.getItem('user-profile');
-      if (!persisted)
-        return;
-      this.profile = JSON.parse(persisted);
-      this.isAuthenticated = true;
-    },
-    userStoreLogout() {
-      this.isAuthenticated = false;
-      this.profile = null;
-      sessionStorage.removeItem('user-profile');
-    },
-  },
+export const useUserStore = defineStore('user', () => {
+  const isAuthenticated = ref(false);
+  const profile = ref(null);
+  const favouritesIDs = ref([]);
+
+  function setProfile(profileData) {
+    profile.value = profileData;
+    isAuthenticated.value = true;
+    sessionStorage.setItem('user-profile', JSON.stringify(profileData));
+  }
+
+  function addFavouriteProduct(id) {
+    favouritesIDs.value.push(id);
+  }
+
+  function removeFavouriteProduct(id) {
+    favouritesIDs.value = favouritesIDs.value.filter(favourite => favourite !== id);
+  }
+
+  function getPersistedProfile() {
+    const persisted = sessionStorage.getItem('user-profile');
+    if (!persisted)
+      return;
+    profile.value = JSON.parse(persisted);
+    isAuthenticated.value = true;
+  }
+
+  function userStoreLogout() {
+    isAuthenticated.value = false;
+    profile.value = null;
+    sessionStorage.removeItem('user-profile');
+  }
+
+  return {
+    isAuthenticated,
+    profile,
+    favouritesIDs,
+    setProfile,
+    addFavouriteProduct,
+    removeFavouriteProduct,
+    getPersistedProfile,
+    userStoreLogout,
+  };
 });
